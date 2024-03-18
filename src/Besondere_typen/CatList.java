d6499e87033988e426fc91b5e873ca4423e6c20d
JanisPirktl <142509118+JanisPirktl@users.noreply.github.com>
2024-03-18 22:39:01 +0100

    fasd

--- src/Besondere_typen/CatList.java
+++ src/Besondere_typen/CatList.java
@@ -7,24 +7,21 @@ public class CatList
 {
   // Add the "count" method we just discussed
   // and fix the code if necessary
-  public static <T> int count( List<T> lst, Predicate<T> predicate )
-  {
+  public static <T> int count( List<T> lst, Predicate<T> predicate ) {
     int result = 0 ;
-    for( T element : lst )
-    {
-      if( predicate.test(element) ) // Die Methode im Predicate heisst test
-      {
+
+    for( T element : lst ) {
+      if( predicate.test(element) ) {
         ++result ;
       }
     }
+
     return result ;
   }
 
 
-  public static void main( String[] args )
-  {
-    ArrayList<Cat> cats = new ArrayList<>( List.of(new Cat("Felix",2500),new Cat("Susi",1800),new Cat("Garfield",5000)) ) ;
-
+  public static void main( String[] args ) {
+    ArrayList<Cat> cats = new ArrayList<>( List.of(new Cat("Felix",2500),new Cat("Susi",1800),new Cat("Garfield",5000)));
 
     // Print all the cats
     cats.forEach(cat -> System.out.println("Name: " + cat.getName() + ", Weight: " + cat.getWeight() + " grams"));
@@ -33,61 +30,44 @@ public class CatList
 
     // Define a lambda to compare the weight of two cats
     Comparator<Cat> compareByWeight = (Cat cat1, Cat cat2) -> cat1.getWeight() - cat2.getWeight();
-    Comparator<Cat> compareByWeight2 = (c1, c2) -> Integer.compare(c1.getWeight(), c2.getWeight());
-
-    // This is not a lambda
-    Comparator<Cat> compareByWeight3 = Comparator.comparingInt(Cat::getWeight);
+    Comparator<Cat> compareByWeight2 = (c1, c2) -> Integer.compare(c1.getWeight(), c2.getWeight()); //besser als 1.
+    Comparator<Cat> compareByWeight3 = Comparator.comparingInt(Cat::getWeight);    // This is not a lambda
 
 
     // Sort the cats by weight, ascending
+    cats.sort(compareByWeight2); //mit der eigenen Funktion oben
     cats.sort(Comparator.comparingInt(Cat::getWeight));
-
-
-
-    // Print the cats
-    cats.forEach(System.out::println);
 
 
     // Sort the cats by weight, descending
     cats.sort(Comparator.comparingInt(Cat::getWeight).reversed());
-
-
-    // Print the cats
-    cats.forEach(System.out::println);
+    cats.sort(compareByWeight2.reversed()); // mit der eigenen Funktion oben
 
 
     // Try to use comparing to sort by name
     cats.sort(Comparator.comparing(Cat::getName));
 
 
-    // Print the cats
-    cats.forEach(System.out::println);
-
-
     // Count the cats whose name is longer than 4 characters and print the result
     System.out.println(count(cats, c -> c.getName().length() > 4));
 
 
-
     // Count the cats whose name starts with 'X' and print the result
     System.out.println(count(cats, c -> c.getName().charAt(0) == 'X'));
-    System.out.println(count(cats, c -> c.getName().startsWith("X")));
+    System.out.println(count(cats, c -> c.getName().startsWith("X"))); //besser
 
 
     // Count the cats whose name ends with 'x' and print the result
     System.out.println(count(cats, c -> c.getName().charAt(c.getName().length()-1) == 'x'));
-    System.out.println(count(cats, c -> c.getName().endsWith("x")));
-
+    System.out.println(count(cats, c -> c.getName().endsWith("x"))); //besser
 
 
     // Count the cats weighing between 2000 and 4000 and print the result
     System.out.println(count(cats, c -> 2000 < c.getWeight() && c.getWeight() < 4000));
 
 
-
     // Define a named lambda which tests if a cat weighs more than 1000
     Predicate<Cat> weighsMoreThan1000 = cat -> cat.getWeight() > 1000;
-
 
 
     // Use the lambda you just defined and the "and"-method in the interface
@@ -95,28 +75,18 @@ public class CatList
     Predicate<Cat> weighsLessThan3000 = cat -> cat.getWeight() < 3000;
 
     System.out.println(count(cats, weighsMoreThan1000.and(weighsLessThan3000) ));
-
+    System.out.println((count(cats, ((Predicate<Cat>) c -> c.getWeight() > 1000).and(c -> c.getWeight() < 3000))));
     System.out.println(count(cats, weighsMoreThan1000.and(cat -> cat.getWeight() < 3000) ));
-
 
 
     // Remove cats weighing over 4000
     cats.removeIf(cat -> cat.getWeight() > 4000);
 
 
-
-    // Print the remaining cats
-    cats.forEach(System.out::println);
-
-
     int gain = 100 ;
     // Increase the weight of the cats by gain
     cats.forEach(cat -> cat.setWeight(cat.getWeight() + gain));
-
     cats.replaceAll(cat -> new Cat(cat.getName(), cat.getWeight() + gain));
 
-
-    // Print the cats
-    cats.forEach(System.out::println);
   }
 }
--- src/Besondere_typen/IntList.java
+++ src/Besondere_typen/IntList.java
@@ -9,125 +9,49 @@ public class IntList
   public static void main( String[] args )
   {
     // Don't worry, we will study this later. It creates an ArrayList containing the Integers 0..100
-    ArrayList<Integer> list = IntStream.range( 0, 101 ).boxed().collect( Collectors.toCollection(ArrayList::new) ) ;
+    ArrayList<Integer> list =
+        IntStream.range( 0, 101 ).boxed().collect( Collectors.toCollection(ArrayList::new) ) ;
 
-    System.out.println( "The full list:" ) ;
     // Print all elements in the list to the console (using a lambda, of course)
-    list.forEach(i -> System.out.println(i));
+    list.forEach(num -> System.out.println(num));
     list.forEach(System.out::println);
 
-
-    list.forEach(e -> System.out.println(e%2==0));
-    
-    System.out.println( "\nRemoving all elements which are divisible by 3\n" ) ;
     // Remove all elements from the list which are divisible by 3
-    list.removeIf(i -> i % 3 == 0);
-    
+    list.removeIf(num -> num % 3 == 0);
 
     // Print the number of elements in the list
     System.out.println(list.size());
 
-
-
-    System.out.println( "The remaining elements:" ) ;
-    // Print all elements in the list to the console (using a lambda, of course)
-    list.forEach(System.out::println);
-    
-    
-    
-    System.out.println( "\nRandomizing the list\n" ) ;
     // Randomize the list using shuffle() -> no lambda needed
     Collections.shuffle(list);
-
-    
-    
-    System.out.println( "The randomized list:" ) ;
-    // Print all elements in the list to the console (using a lambda, of course)
-    list.forEach(System.out::println);
-
-
 
     // Sort the list according to the natural order
     list.sort(Comparator.comparingInt(Integer::intValue));
     list.sort((Comparator.naturalOrder())); //best one
-    list.sort((i1, i2) -> Integer.compare(i1, i2));
-
-
-
-
-    System.out.println( "The sorted list:" ) ;
-    // Print all elements in the list to the console (using a lambda, of course)
-    list.forEach(System.out::println);
-
-
+    list.sort((num1, num2) -> Integer.compare(num1, num2));
+    list.sort(Integer::compare);
 
     // Sort the list according in reverse natural order
     list.sort(Comparator.comparingInt(Integer::intValue).reversed());
-    list.sort((Comparator.reverseOrder()));
-    list.sort((i1, i2) -> Integer.compare(i2, i1));
-
-
-
-    System.out.println( "The sorted list (reverse):" ) ;
-    // Print all elements in the list to the console (using a lambda, of course)
-    list.forEach(System.out::println);
-
-
+    list.sort((Comparator.reverseOrder())); //best one
+    list.sort((num1, num2) -> Integer.compare(num2, num1));
 
     // Sort the list such that elements which are divisible by 2 come first
-    list.sort((i1, i2) -> Integer.compare(i1, i2 % 2));
-
-
-
-    System.out.println( "The sorted list (even numbers first):" ) ;
-    // Print all elements in the list to the console (using a lambda, of course)
-    list.forEach(System.out::println);
+    // (a, b) -> (a % 2) - (b % 2)     is identical to     Comparator.comparingInt(a -> (a % 2))
+    Comparator<Integer> evenFirstComparator = Comparator.comparingInt(a -> (a % 2));
+    list.sort(evenFirstComparator);
 
 
 
     // Sort the list such that elements which are divisible by 2 come first
     // and the numbers in both groups are sorted ascending
-    Comparator<Integer> even = (a,b) -> (a % 2) - (b % 2);
-
-    list.sort((i1, i2) -> {
-      if ((i1 % 2 == 0) == (i2 % 2 == 0)) { // Both are even or odd
-        return Integer.compare(i1, i2); // Sort in ascending order
-      } else {
-        return Boolean.compare(i2 % 2 == 0, i1 % 2 == 0); // Even numbers come first
-      }
-    });
+    list.sort(evenFirstComparator.thenComparing(Comparator.naturalOrder()));
 
 
+    //Removing all elements greater than 24 and less than 42
+    list.removeIf(num -> 24 < num && num < 42);
 
-    System.out.println( "The sorted list (even numbers first):" ) ;
-    // Print all elements in the list to the console (using a lambda, of course)
-    list.forEach(System.out::println);
-
-
-
-    System.out.println( "\nRemoving all elements greater than 24 and less than 42\n" ) ;
-    // Write the code
-    list.removeIf(i -> 24 < i && i < 42);
-
-
-
-    // Print the number of elements in the list
-    System.out.println( "The remaining elements:" ) ;
-    // Print all elements in the list to the console (using a lambda, of course)
-    list.forEach(System.out::println);
-
-
-
-    System.out.println( "\nRemoving all elements which are less than 12 or greater than 88\n" ) ;
-    // Write the code
-    list.removeIf(i -> 12 > i || 88 < i);
-
-
-
-    // Print the number of elements in the list
-
-    System.out.println( "The remaining elements:" ) ;
-    // Print all elements in the list to the console (using a lambda, of course)
-    list.forEach(System.out::println);
+    //Removing all elements which are less than 12 or greater than 88
+    list.removeIf(num -> 12 > num || 88 < num);
   }
 }
